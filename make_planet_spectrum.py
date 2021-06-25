a034c54014440fdff75f00f4724612cd747098e2
Evan Anders <evanhanders@gmail.com>
2021-06-25 13:41:30 -0500

    makes a planetary config file / parsing system; lines are added based on abundance in planetary atmosphere.

--- make_planet_spectrum.py
+++ make_planet_spectrum.py
@@ -8,10 +8,29 @@ L = absorption path length
 n = density of absorber
 L * n = N = column density
 
+Usage:
+    make_planet_spectrum.py <planet>
+
 """
+from configparser import ConfigParser
+from pathlib import Path
+
+from docopt import docopt
 import numpy as np
 from scipy.special import voigt_profile
 import matplotlib.pyplot as plt
+
+args = docopt(__doc__)
+#Read planet file
+if args['<planet>'] is not None: 
+    planet_file = Path(args['<planet>'])
+    planet = ConfigParser()
+    planet.read(str(planet_file))
+    elements = dict(planet.items('elements'))
+for k, frac in elements.items():
+    elements[k] = float(frac)
+    
+
 
 c = 3.0e8 #m/s
 nm_to_m = 1e-9
@@ -24,24 +43,21 @@ def add_line(wavelength, intensity, column_density, atmo_velocity, line_center, 
 hydrogen = np.genfromtxt('line_files/hydrogen.csv', delimiter=',', skip_header=2)
 helium = np.genfromtxt('line_files/helium.csv', delimiter=',', skip_header=2)
 
-cols = [1e17]
+cols = [1e17, 1e12]
 atmo_sigma = 1e12
 
 lambdas = np.linspace(1, 1000, 10000)
 
 for N in cols:
     intensities = np.ones_like(lambdas)
-    h_intensities = np.ones_like(lambdas)
-    for i in range(hydrogen.shape[0]):
-        line = tuple(hydrogen[i,:])
-        intensities = add_line(lambdas, intensities, N, atmo_sigma, *line)
-        h_intensities = add_line(lambdas, h_intensities, N, atmo_sigma, *line)
-    for i in range(helium.shape[0]):
-        line = tuple(helium[i,:])
-        intensities = add_line(lambdas, intensities, N, atmo_sigma, *line)
+    for ele, ele_frac in elements.items():
+        ele_data = np.genfromtxt('line_files/{}.csv'.format(ele), delimiter=',', skip_header=2)
+        N_ele = ele_frac*N
+        for i in range(ele_data.shape[0]):
+            line = tuple(hydrogen[i,:])
+            intensities = add_line(lambdas, intensities, N, atmo_sigma, *line)
 
     plt.plot(lambdas, intensities, label='N={}'.format(N))
-    plt.plot(lambdas, h_intensities, label='N={}'.format(N))
 plt.show()
     
 
